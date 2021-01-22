03fb745686a9012ba140ce665f3dee9515970d15
Helio <helio.fototerra@gmail.com>
2021-01-22 09:52:14 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import { history } from './_helpers';
+
+function App() {
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <div className="col-md-8 offset-md-2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
